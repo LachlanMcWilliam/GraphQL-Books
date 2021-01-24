873fe448cfec93e4da5c195d1f005b64c509d254
Lachlan <mcwilliam01@gmail.com>
2021-01-24 09:50:19 +0000

    Added mongodb to the project and connected using mongo

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
 import { BookSchema } from "./schema/schema.js";
 import { graphqlHTTP } from "express-graphql";
 
 if (!process.env.SERVICE_PORT) {
   throw new Error("Please define SERVICE_PORT in the environment variables");
+}
+if (!process.env.MONGO_URI) {
+  throw new Error("Please define MONGO_URI in the environment variables");
 }
 
 const app = express();
@@ -16,6 +20,16 @@ app.use(
   })
 );
 
-app.listen(process.env.SERVICE_PORT, () => {
+app.listen(process.env.SERVICE_PORT, async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Mongoose connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
   console.log(`GraphQL server listening on port ${process.env.SERVICE_PORT}`);
 });
